2fec094438ad1f1f3722de8413473310c5ceb187
Jeffrey Davis <j.davis081248@gmail.com>
2024-01-08 12:53:20 -0600

    Done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    answer = []
+    f = 0
+    s = 1
+
+    while len(answer) < length:
+        answer.append(f)
+        t = f
+        f = s
+        s = t + s
+
+    print(answer)
+
+print_fibonacci(3)
